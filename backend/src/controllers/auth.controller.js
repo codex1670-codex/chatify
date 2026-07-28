139bc4578f112c60b267f7c480d2475526382cd2
sanjiv <shankarik11@gamil.com>
2026-07-28 22:47:57 +0530

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -68,13 +68,17 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-    const {email, password} = req.body
+    const {email, password} = req.body;
+
+    if (!email || !password){
+        return res.status(400).json({message:"Email and password are required"});
+    }
     try{
-        const user = await User.findOne({email})
+        const user = await User.findOne({email});
         if (!user) return res.status(400).json({message:"Invalid credentials"});
 
 
-        const isPasswordCorrect = await bcrypt.compare(password,user.password)
+        const isPasswordCorrect = await bcrypt.compare(password,user.password);
         if(!isPasswordCorrect) return res.status(400).json({message:"Invalid credentials"});
 
         generateToken(user._id, res)
